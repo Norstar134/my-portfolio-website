61bab9d04c5cdf8fa17ec00be15955a84bd7db79
Norstar134 <norstar134@gmail.com>
2024-08-14 13:51:29 +0100

    Contact form added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -200,7 +200,7 @@ export default function Home() {
                 <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                   <img className="object-contain w-full rounded-t-lg h-60 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="\timelogging.PNG" alt="" />
                   <div className="flex flex-col justify-between p-4 leading-normal">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Time Logging Application for Wee Tattie Studio's</h5>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Time Logging Application for Wee Tattie Studio</h5>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">This is the biggest project I've worked on so far and is helping me expand my tech stack. The aim of this project is to allow
                       developers to record their hours to be compensated in the future once the studio gets funding.
                     </p>
@@ -214,11 +214,24 @@ export default function Home() {
       </section>
 
       <section id="contact" className="flex-col">
-        <div className="pt-16">
-          <div className="pr-24">
+        <div className="py-8 md:py-16 px-4 mx-auto max-w-screen-md">
             <span className="text-3xl text-[#80ccff] font-bold">CONTACT ME</span>
+            <form action="#" className="space-y-8 pt-10">
+              <div>
+                <label htmlFor="email" className="block mb-2 text-base font-medium md:text-lg text-gray-900 dark:text-gray-300">Your email</label>
+                <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="name@email.com" required />
+              </div>
+              <div>
+                <label htmlFor="mesgSubject" className="block mb-2 text-base font-medium md:text-lg text-gray-900 dark:text-gray-300">Subject</label>
+                <input type="text" id="mesgSubject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm" placeholder="Let me know what you want to talk about" required />
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="message" className="block mb-2 text-base font-medium md:text-lg text-gray-900 dark:text-gray-400">Your message</label>
+                <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300" placeholder="Leave a comment..."></textarea>
+              </div>
+              <button type="submit" className="text-sm text-center bg-[#80ccff] hover:bg-[#00ace6] text-white font-bold py-4 px-7 rounded-full">Send message</button>
+            </form>
           </div>
-        </div>
       </section>
 
       </div>
